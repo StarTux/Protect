673986131bfd855194d9acc1a69a760ab66859f3
StarTux <ths.loy@googlemail.com>
2022-08-03 13:20:17 +0200

    Remove legacy features

--- src/main/java/com/winthier/protect/ProtectPlugin.java
+++ src/main/java/com/winthier/protect/ProtectPlugin.java
@@ -5,11 +5,7 @@ import com.cavetale.core.event.block.PlayerBreakBlockEvent;
 import com.cavetale.core.event.entity.PlayerEntityAbilityQuery;
 import com.destroystokyo.paper.MaterialTags;
 import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.World;
@@ -58,8 +54,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ProtectPlugin extends JavaPlugin implements Listener {
     private final List<String> worlds = new ArrayList<>();
-    private final Set<Block> farmBlocks = new HashSet<>();
-    private final Set<Material> farmMaterials = EnumSet.noneOf(Material.class);
 
     @Override
     public void onEnable() {
@@ -69,98 +63,19 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command,
-                             String alias, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
         if (args.length == 1 && args[0].equals("reload")) {
             importConfig();
             sender.sendMessage("Config reloaded");
-            return true;
-        } else if (args.length == 1
-                   && (args[0].equals("addfarmblocks")
-                       || args[0].equals("removefarmblocks"))) {
-            if (!(sender instanceof Player)) return false;
-            Player player = (Player) sender;
-            boolean add = args[0].startsWith("add");
-            int ax;
-            int ay;
-            int az;
-            int bx;
-            int by;
-            int bz;
-            try {
-                ax = getSelectionMeta(player, "SelectionAX");
-                ay = getSelectionMeta(player, "SelectionAY");
-                az = getSelectionMeta(player, "SelectionAZ");
-                bx = getSelectionMeta(player, "SelectionBX");
-                by = getSelectionMeta(player, "SelectionBY");
-                bz = getSelectionMeta(player, "SelectionBZ");
-            } catch (NullPointerException npe) {
-                sender.sendMessage("No selection made.");
-                return true;
-            }
-            World world = getServer().getWorld(worlds.get(0));
-            int count = 0;
-            for (int y = Math.min(ay, by); y <= Math.max(ay, by); y += 1) {
-                for (int z = Math.min(az, bz); z <= Math.max(az, bz); z += 1) {
-                    for (int x = Math.min(ax, bx); x <= Math.max(ax, bx); x += 1) {
-                        Block block = world.getBlockAt(x, y, z);
-                        if (add) {
-                            farmBlocks.add(block);
-                            count += 1;
-                        } else {
-                            if (farmBlocks.remove(block)) count += 1;
-                        }
-                    }
-                }
-            }
-            saveFarmBlocks();
-            if (add) {
-                player.sendMessage("" + count + " farm blocks added.");
-            } else {
-                player.sendMessage("" + count + " farm blocks removed.");
-            }
             return true;
         }
         return false;
     }
 
-    void importConfig() {
+    private void importConfig() {
         reloadConfig();
         worlds.clear();
         worlds.addAll(getConfig().getStringList("worlds"));
-        farmMaterials.clear();
-        for (String key: getConfig().getStringList("farm-materials")) {
-            try {
-                Material mat = Material.valueOf(key.toUpperCase());
-                farmMaterials.add(mat);
-            } catch (IllegalArgumentException iae) {
-                iae.printStackTrace();
-            }
-        }
-        farmBlocks.clear();
-        if (worlds.isEmpty()) return;
-        World world = getServer().getWorld(worlds.get(0));
-        if (world == null) return;
-        Iterator<Integer> is = getConfig().getIntegerList("farm-blocks").iterator();
-        while (is.hasNext()) {
-            int x = is.next();
-            int y = is.next();
-            int z = is.next();
-            farmBlocks.add(world.getBlockAt(x, y, z));
-        }
-        getLogger().info("Protecting worlds: " + worlds + ", "
-                         + farmBlocks.size() + " farm blocks.");
-    }
-
-    void saveFarmBlocks() {
-        List<Integer> ls = new ArrayList<>();
-        for (Block block: farmBlocks) {
-            ls.add(block.getX());
-            ls.add(block.getY());
-            ls.add(block.getZ());
-        }
-        getConfig().set("farm-blocks", ls);
-        saveConfig();
     }
 
     /**
@@ -186,7 +101,7 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    private void onPlayerInteract(PlayerInteractEvent event) {
         if (!worlds.contains(event.getPlayer().getWorld().getName())) return;
         Block block = event.getClickedBlock();
         if (event.getAction() == Action.PHYSICAL) {
@@ -198,8 +113,6 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
                 return;
             }
         }
-        if (farmBlocks.contains(block)) return;
-        if (farmBlocks.contains(block.getRelative(0, 1, 0))) return;
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             Material mat = block.getType();
             if (Tag.DOORS.isTagged(mat)) return;
@@ -228,51 +141,42 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onBlockPlace(BlockPlaceEvent event) {
-        if (farmBlocks.contains(event.getBlock())
-            && farmMaterials.contains(event.getBlock().getType())) {
-            return;
-        }
+    private void onBlockPlace(BlockPlaceEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onBlockBreak(BlockBreakEvent event) {
-        if (farmBlocks.contains(event.getBlock())
-            && farmMaterials.contains(event.getBlock().getType())) {
-            return;
-        }
+    private void onBlockBreak(BlockBreakEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onBlockDamage(BlockDamageEvent event) {
-        if (farmBlocks.contains(event.getBlock())) return;
+    private void onBlockDamage(BlockDamageEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     // Frost Walker
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onEntityBlockForm(EntityBlockFormEvent event) {
+    private void onEntityBlockForm(EntityBlockFormEvent event) {
         Player player = event.getEntity() instanceof Player ? (Player) event.getEntity() : null;
         if (player == null) return;
         onProtectEvent(player, event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+    private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
         if (event.getRightClicked().getType() == EntityType.PIG) return;
         if (event.getRightClicked().getType() == EntityType.BOAT) return;
         onProtectEvent(event.getPlayer(), null, event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
+    private void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
         if (event.getRightClicked().getType() == EntityType.PIG) return;
         onProtectEvent(event.getPlayer(), null, event);
     }
 
-    Player getPlayerDamager(Entity damager) {
+    private Player getPlayerDamager(Entity damager) {
         if (damager instanceof Player) {
             return (Player) damager;
         }
@@ -285,14 +189,14 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+    private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
         Player player = getPlayerDamager(event.getDamager());
         if (player == null) return;
         onProtectEvent(player, null, event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onVehicleDamage(VehicleDamageEvent event) {
+    private void onVehicleDamage(VehicleDamageEvent event) {
         if (event.getAttacker() == null) return;
         Player player = getPlayerDamager(event.getAttacker());
         if (player == null) return;
@@ -305,7 +209,7 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
      * otherwise (including ProjectileHit and Collide).
      */
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onProjectileLaunch(ProjectileLaunchEvent event) {
+    private void onProjectileLaunch(ProjectileLaunchEvent event) {
         Player player = getPlayerDamager(event.getEntity());
         if (player == null) return;
         if (!onProtectEvent(player, null, null)) {
@@ -317,26 +221,26 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onEntityCombustByEntity(EntityCombustByEntityEvent event) {
+    private void onEntityCombustByEntity(EntityCombustByEntityEvent event) {
         Player player = getPlayerDamager(event.getCombuster());
         if (player == null) return;
         onProtectEvent(player, null, event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onEntityExplode(EntityExplodeEvent event) {
+    private void onEntityExplode(EntityExplodeEvent event) {
         if (!worlds.contains(event.getEntity().getWorld().getName())) return;
         event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onBlockExplode(BlockExplodeEvent event) {
+    private void onBlockExplode(BlockExplodeEvent event) {
         if (!worlds.contains(event.getBlock().getWorld().getName())) return;
         event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onBlockIgnite(BlockIgniteEvent event) {
+    private void onBlockIgnite(BlockIgniteEvent event) {
         if (!worlds.contains(event.getBlock().getWorld().getName())) return;
         switch (event.getCause()) {
         case FLINT_AND_STEEL: return;
@@ -346,7 +250,7 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onCauldronLevelChange(CauldronLevelChangeEvent event) {
+    private void onCauldronLevelChange(CauldronLevelChangeEvent event) {
         if (!worlds.contains(event.getBlock().getWorld().getName())) return;
         switch (event.getReason()) {
         case EVAPORATE:
@@ -357,14 +261,14 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onHangingBreak(HangingBreakEvent event) {
+    private void onHangingBreak(HangingBreakEvent event) {
         if (event instanceof HangingBreakByEntityEvent) return;
         if (!worlds.contains(event.getEntity().getWorld().getName())) return;
         event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onHangingBreakByEntity(HangingBreakByEntityEvent event) {
+    private void onHangingBreakByEntity(HangingBreakByEntityEvent event) {
         if (event.getRemover() instanceof Player) {
             onProtectEvent((Player) event.getRemover(), null, event);
         } else {
@@ -374,33 +278,27 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onHangingPlace(HangingPlaceEvent event) {
+    private void onHangingPlace(HangingPlaceEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
+    private void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerBucketFill(PlayerBucketFillEvent event) {
+    private void onPlayerBucketFill(PlayerBucketFillEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
-    public void onPlayerBreakBlock(PlayerBreakBlockEvent event) {
-        if (farmBlocks.contains(event.getBlock())) return;
+    private void onPlayerBreakBlock(PlayerBreakBlockEvent event) {
         onProtectEvent(event.getPlayer(), event.getBlock(), event);
     }
 
-    // @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    // public void onPlayerCanDamageEntity(PlayerCanDamageEntityEvent event) {
-    //     onProtectEvent(event.getPlayer(), event);
-    // }
-
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerEggThrow(PlayerEggThrowEvent event) {
+    private void onPlayerEggThrow(PlayerEggThrowEvent event) {
         final Player player = event.getPlayer();
         if (player.isOp()) return;
         if (player.hasPermission("protect.override")) return;
@@ -411,7 +309,7 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+    private void onEntityChangeBlock(EntityChangeBlockEvent event) {
         if (worlds.contains(event.getBlock().getWorld().getName())) {
             if (event.getEntity() instanceof Player
                 && event.getBlock().getType() == Material.BIG_DRIPLEAF
@@ -423,18 +321,18 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerFish(PlayerFishEvent event) {
+    private void onPlayerFish(PlayerFishEvent event) {
         if (event.getCaught() == null) return;
         onProtectEvent(event.getPlayer(), null, event);
     }
 
     @EventHandler
-    public void onPlayerTakeLecternBook(PlayerTakeLecternBookEvent event) {
+    private void onPlayerTakeLecternBook(PlayerTakeLecternBookEvent event) {
         onProtectEvent(event.getPlayer(), null, event);
     }
 
     @EventHandler(ignoreCancelled = true)
-    void onPlayerBlockAbility(PlayerBlockAbilityQuery query) {
+    private void onPlayerBlockAbility(PlayerBlockAbilityQuery query) {
         switch (query.getAction()) {
         case USE: // Buttons: Doors
         case READ: // Lectern
@@ -443,9 +341,6 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
         case FLY:
             return;
         case BUILD:
-            if (farmBlocks.contains(query.getBlock()) && farmMaterials.contains(query.getBlock().getType())) {
-                return;
-            }
         case PLACE_ENTITY:
         case SPAWN_MOB: // PocketMob
         default:
@@ -454,7 +349,7 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true)
-    void onPlayerEntityAbility(PlayerEntityAbilityQuery query) {
+    private void onPlayerEntityAbility(PlayerEntityAbilityQuery query) {
         switch (query.getAction()) {
         case MOUNT:
         case DISMOUNT:
@@ -477,27 +372,20 @@ public final class ProtectPlugin extends JavaPlugin implements Listener {
         }
     }
 
-    // --- Meta Utility
-
-    int getSelectionMeta(Player player, String key) {
-        if (!player.hasMetadata(key)) throw new NullPointerException();
-        return player.getMetadata(key).get(0).asInt();
-    }
-
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    void onBlockForm(BlockFormEvent event) {
+    private void onBlockForm(BlockFormEvent event) {
         String worldName = event.getBlock().getWorld().getName();
         if (worlds.contains(worldName)) event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    void onBlockFade(BlockFadeEvent event) {
+    private void onBlockFade(BlockFadeEvent event) {
         String worldName = event.getBlock().getWorld().getName();
         if (worlds.contains(worldName)) event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
-    void onBlockFromTo(BlockFromToEvent event) {
+    private void onBlockFromTo(BlockFromToEvent event) {
         String worldName = event.getBlock().getWorld().getName();
         if (worlds.contains(worldName)) event.setCancelled(true);
     }
